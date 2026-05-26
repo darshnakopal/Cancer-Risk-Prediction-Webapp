bf4268baaf24e9ce27df826d201c80ae26d23974
Darshna Kopal <darshnakopal@gmail.com>
2026-05-27 02:19:32 +0800

    minor changes added

--- pages/1_Background.py
+++ pages/1_Background.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="Background")
 st.header("Background", divider="red")
 st.write("""
 Cancer is a leading cause of death worldwide, with millions of new cases diagnosed each year. 
-         Early detection and accurate risk prediction are crucial for improving patient outcomes and reducing the burden on healthcare systems. 
+         Early detection and accurate risk prediction are crucial for improving patient outcomes and reducing the burden on healthcare systems now. 
          Once called "a disease of the rich", cancer has now become more prevalent than ever.
          This has largely been due to the concentration of **carcinogens** that are present in our daily lives, such as air we breathe, food we eat, and water we drink.
          As we grow older, the need for preventative care increases as well as a greater awareness to the amount of harm we present to the body.
